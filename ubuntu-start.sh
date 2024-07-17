f339aa945a338c1242f7395781bc24083b7441db
Gábor Gyebnár <korteur@gmail.com>
2024-07-17 11:19:17 +0000

    Fixes fd install

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -4,10 +4,13 @@ sudo apt update
 sudo apt install build-essential unzip pkg-config libssl-dev cmake -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-cargo install starship bat fd
+cargo install starship bat fd-find
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 eval "$(starship init bash)"
 
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
 #starship preset pastel-powerline -o ~/.config/starship.toml
+
+# Java 17
+# sudo apt-get install openjdk-17-jdk
