4e9f86f28a02491f4629e55c063318a6b55cc355
Gábor Gyebnár <korteur@gmail.com>
2024-07-17 11:31:38 +0000

    Adds pip and venv

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -19,5 +19,5 @@ starship preset gruvbox-rainbow -o ~/.config/starship.toml
 # sudo apt install software-properties-common
 # sudo add-apt-repository ppa:deadsnakes/ppa
 # sudo apt update
-# sudo apt install python3.10
+# sudo apt install python3.10 python3-pip python3.10-venv
 
