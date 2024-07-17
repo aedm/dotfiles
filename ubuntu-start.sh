63d24907fc00ddd7bde7253ec802d41c1d8d8816
Gábor Gyebnár <korteur@gmail.com>
2024-07-17 15:05:02 +0000

    Adds cargo install fix

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install build-essential unzip pkg-config libssl-dev cmake -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+. "$HOME/.cargo/env"
 cargo install starship bat fd-find
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
